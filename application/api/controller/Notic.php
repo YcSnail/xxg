0004f13d4af1003c09972083bd7d356883e6cf97
yc <yc@yuanxu.top>
2018-07-30 02:05:21 +0800

    添加 notic API 接口

---
+++ application/api/controller/Notic.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace app\index\controller;
+
+
+class notic {
+
+
+    // 查询返回所有数据
+    public function index(){
+
+        // 获取当前分页
+        // 默认 当前页为0
+        // 每页10条数据
+        $page = 0;
+        $pageNum = 10;
+
+        // 分页返回
+
+
+    }
+
+    // 获取指定数据
+    public function get(){
+
+    }
+
+    // 添加数据
+    public function add(){
+
+    }
+
+    // 修改数据
+    public function edit(){
+
+    }
+
+
+}
